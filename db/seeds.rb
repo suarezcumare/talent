b1cd71c4ffd88695a2a7aa43e61a1453abb4a8d2
Tony Suarez <suarezcumare@gmail.com>
2017-06-22 08:47:27 -0400

    add seed de migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+City.create(code:"3884448", name:"Los Condes CL")
+City.create(code:"3875139", name:"Providencia CL")
+City.create(code:"3873544", name:"Region Metropolitana CL")
+City.create(code:"3649833", name:"Acarigua VE")
+City.create(code:"3649833", name:"Acarigua VE")
+City.create(code:"3648522", name:"Barquisimeto VE")
+City.create(code:"3646738", name:"Caracas VE")
