c728ba5f3a24b9c5764d55a9dd1aa348548d340a
Tony Suarez <suarezcumare@gmail.com>
2017-06-22 09:44:23 -0400

    repetido acarigua

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+City.destroy_all
+Temperature.destroy_all
 City.create(code:"3884448", name:"Los Condes CL")
 City.create(code:"3875139", name:"Providencia CL")
 City.create(code:"3873544", name:"Region Metropolitana CL")
-City.create(code:"3649833", name:"Acarigua VE")
 City.create(code:"3649833", name:"Acarigua VE")
 City.create(code:"3648522", name:"Barquisimeto VE")
 City.create(code:"3646738", name:"Caracas VE")
